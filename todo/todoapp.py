e2b608a36d924c990a3ee80f4341d3c0115d8e1b
oscar89 <oskr_89@live.com.ar>
2023-12-24 20:06:56 +0100

    comenzando a listar las tareas en el html

--- todo/todoapp.py
+++ todo/todoapp.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from ToDo.aut import login_required
+from ToDo.models import User, Todo
+from ToDo import db
 
 bp = Blueprint("app", __name__, url_prefix="/app")
 
@@ -7,4 +9,10 @@ bp = Blueprint("app", __name__, url_prefix="/app")
 @bp.route("/list")
 @login_required
 def lista():
-    return render_template("todo/index.html")
+    todos = Todo.query.all()
+    return render_template("todo/index.html", tareas=todos)
+
+
+@bp.route("/crear")
+def crear():
+    return render_template("todo/crear.html")
