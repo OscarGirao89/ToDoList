07fbd6b6eab4b4d6d5693f6a71f9c75da8fc11fe
oscar89 <oskr_89@live.com.ar>
2023-12-24 01:07:48 +0100

    ya se pueden crear usuarios

--- todo/aut.py
+++ todo/aut.py
@@ -7,8 +7,22 @@ from ToDo import db
 bp = Blueprint("aut", __name__, url_prefix="/aut")
 
 
-@bp.route("/registro")
+@bp.route("/registro", methods=["GET", "POST"])
 def registro():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        usuario = User(username, email, generate_password_hash(password))
+
+        nombre_usuario = User.query.filter_by(username=username).first()
+        if nombre_usuario == None:
+            db.session.add(usuario)
+            db.session.commit()
+
+            return redirect(url_for("aut.acceder"))
+
     return render_template("aut/registro.html")
 
 
