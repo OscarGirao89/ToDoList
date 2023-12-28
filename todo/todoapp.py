80977e89ca707cbbeb9030368b17e3546402a8b3
oscar89 <oskr_89@live.com.ar>
2023-12-28 01:03:42 +0100

    se agrego editar y eliminar tareas

--- todo/todoapp.py
+++ todo/todoapp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session, g
+from flask import Blueprint, render_template, request, session, g, redirect, url_for
 from ToDo.aut import login_required
 from ToDo.models import User, Todo
 from ToDo import db
@@ -14,7 +14,7 @@ def lista():
         titulo = request.form["titulo_tarea"]
         desc = request.form["desc_tarea"]
 
-        tarea = Todo(creador, titulo, desc, True)
+        tarea = Todo(creador, titulo, desc)
 
         db.session.add(tarea)
         db.session.commit()
@@ -27,6 +27,24 @@ def lista():
     return render_template("todo/index.html", tareas=todos)
 
 
-@bp.route("/crear")
-def crear():
-    return render_template("todo/crear.html")
+@bp.route("/editar/<int:id>", methods=["POST", "GET"])
+@login_required
+def editar(id):
+    tarea = Todo.query.get_or_404(id)
+    if request.method == "POST":
+        tarea.titulo = request.form["titulo_tarea"]
+        tarea.desc = request.form["desc_tarea"]
+        db.session.commit()
+
+        return redirect(url_for("app.lista"))
+
+    return render_template("todo/editar.html", tarea=tarea)
+
+
+@bp.route("/eliminar/<int:id>", methods=["POST", "GET"])
+@login_required
+def eliminar(id):
+    tarea = Todo.query.get_or_404(id)
+    db.session.delete(tarea)
+    db.session.commit()
+    return redirect(url_for("app.lista"))
